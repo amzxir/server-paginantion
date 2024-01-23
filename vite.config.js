4b1f5c569ff61554cf18059dce03f10aed5e0a44
amzxir <amirreza79ahmadi@gmail.com>
2024-01-23 21:16:59 +0330

    sdasd

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  base: "/client-pagination/",
   server: {
     proxy: {
       '/api': {
@@ -12,6 +14,4 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
-  base: "/client-pagination/"
 })
